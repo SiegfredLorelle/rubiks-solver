5a415b84162076c5ed3d9d0bd8abd83562cfb4af
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2023-07-12 23:38:43 +0800

    Clean up unused code in cube.js

--- www/js/cube.js
+++ www/js/cube.js
@@ -3053,8 +3053,8 @@ class Storage {
     if ( ! userVersion || userVersion !== window.gameVersion ) {
 
       this.clearGame();
-      this.clearPreferences();
-      this.migrateScores();
+      // this.clearPreferences();
+      // this.migrateScores();
       localStorage.setItem( 'theCube_version', window.gameVersion );
 
     }
@@ -3150,34 +3150,10 @@ class Storage {
 
   clearScores() {
 
-    localStorage.removeItem( 'theCube_scores' );
+    // localStorage.removeItem( 'theCube_scores' );
 
   }
 
-  migrateScores() {
-
-    try {
-
-      const scoresData = JSON.parse( localStorage.getItem( 'theCube_scoresData' ) );
-      const scoresBest = parseInt( localStorage.getItem( 'theCube_scoresBest' ) );
-      const scoresWorst = parseInt( localStorage.getItem( 'theCube_scoresWorst' ) );
-      const scoresSolves = parseInt( localStorage.getItem( 'theCube_scoresSolves' ) );
-
-      if ( ! scoresData || ! scoresBest || ! scoresSolves || ! scoresWorst ) return false;
-
-      this.game.scores.data[ 3 ].scores = scoresData;
-      this.game.scores.data[ 3 ].best = scoresBest;
-      this.game.scores.data[ 3 ].solves = scoresSolves;
-      this.game.scores.data[ 3 ].worst = scoresWorst;
-
-      localStorage.removeItem( 'theCube_scoresData' );
-      localStorage.removeItem( 'theCube_scoresBest' );
-      localStorage.removeItem( 'theCube_scoresWorst' );
-      localStorage.removeItem( 'theCube_scoresSolves' );
-
-    } catch( e ) {}
-
-  }
 
   loadPreferences() {
 
@@ -3215,27 +3191,6 @@ class Storage {
       return false;
 
     }
-
-  }
-
-  savePreferences() {
-
-    const preferences = {
-      cubeSize: 3,
-      flipConfig: 0,
-      dificulty: 1,
-      fov: 10,
-      theme: this.game.themes.theme,
-      colors: this.game.themes.colors,
-    };
-
-    localStorage.setItem( 'theCube_preferences', JSON.stringify( preferences ) );
-
-  }
-
-  clearPreferences() {
-
-    localStorage.removeItem( 'theCube_preferences' );
 
   }
 
@@ -3318,12 +3273,6 @@ class Themes {
 
     const colors = this.getColors();
 
-    // this.game.dom.prefs.querySelectorAll( '.range__handle div' ).forEach( range => {
-
-    //   range.style.background = '#' + colors.R.toString(16).padStart(6, '0');
-
-    // } );
-
     this.game.cube.updateColors( colors );
 
     this.game.confetti.updateColors( colors );
@@ -3340,21 +3289,8 @@ class ThemeEditor {
 
     this.game = game;
 
-    this.editColor = 'R';
-
-    this.getPieceColor = this.getPieceColor.bind( this );
-
   }
 
-  colorFromHSL( h, s, l ) {
-
-    h = Math.round( h );
-    s = Math.round( s );
-    l = Math.round( l );
-
-    return new THREE.Color( `hsl(${h}, ${s}%, ${l}%)` );
-
-  }
 
   setHSL( color = null, animate = false ) {
 
@@ -3424,47 +3360,6 @@ class ThemeEditor {
 
   }
 
-  updateHSL() {
-
-    const { hue, saturation, lightness } = this.game.preferences.ranges;
-
-    const h = hue.value;
-    const s = saturation.value;
-    const l = lightness.value;
-
-    const color = this.colorFromHSL( h, s, l ).getStyle();
-
-    hue.handle.style.color = color;
-    saturation.handle.style.color = color;
-    lightness.handle.style.color = color;
-
-    saturation.track.style.color = this.colorFromHSL( h, 100, 50 ).getStyle();
-    lightness.track.style.color = this.colorFromHSL( h, s, 50 ).getStyle();
-
-    this.game.dom.theme.style.display = 'none';
-    this.game.dom.theme.offsetHeight;
-    this.game.dom.theme.style.display = '';
-
-    const theme = this.game.themes.theme;
-
-    this.game.themes.colors[ theme ][ this.editColor ] = this.colorFromHSL( h, s, l ).getHex();
-    this.game.themes.setTheme();
-
-  }
-
-  colorPicker( enable ) {
-
-    if ( enable ) {
-
-      this.game.dom.game.addEventListener( 'click', this.getPieceColor, false );
-
-    } else {
-
-      this.game.dom.game.removeEventListener( 'click', this.getPieceColor, false );
-
-    }
-
-  }
 
   getPieceColor( event ) {
 
@@ -3494,17 +3389,6 @@ class ThemeEditor {
     const name = edgeIntersect ? edgeIntersect.object.name : pieceIntersect ? 'P' : 'G';
 
     this.setHSL( name, true );
-
-  }
-
-  resetTheme() {
-
-    this.game.themes.colors[ this.game.themes.theme ] =
-      JSON.parse( JSON.stringify( this.game.themes.defaults[ this.game.themes.theme ] ) );
-
-    this.game.themes.setTheme();
-
-    this.setHSL( this.editColor, true );
 
   }
 
@@ -3577,111 +3461,6 @@ const States = {
   }
 };
 
-class IconsConverter {
-
-  constructor( options ) {
-
-    options = Object.assign( {
-      tagName: 'icon',
-      className: 'icon',
-      styles: false,
-      icons: {},
-      observe: false,
-      convert: false,
-    }, options || {} );
-
-    this.tagName = options.tagName;
-    this.className = options.className;
-    this.icons = options.icons;
-
-    this.svgTag = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' );
-    this.svgTag.setAttribute( 'class', this.className );
-
-    if ( options.styles ) this.addStyles();
-    if ( options.convert ) this.convertAllIcons();
-
-    if ( options.observe ) {
-
-      const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-      this.observer = new MutationObserver( mutations => { this.convertAllIcons(); } );
-      this.observer.observe( document.documentElement, { childList: true, subtree: true } );
-
-    }
-
-    return this;
-
-  }
-
-  convertAllIcons() {
-
-    document.querySelectorAll( this.tagName ).forEach( icon => { this.convertIcon( icon ); } );
-
-  }
-
-  convertIcon( icon ) {
-
-    const svgData = this.icons[ icon.attributes[0].localName ];
-
-    if ( typeof svgData === 'undefined' ) return;
-
-    const svg = this.svgTag.cloneNode( true );
-    const viewBox = svgData.viewbox.split( ' ' );
-
-    svg.setAttributeNS( null, 'viewBox', svgData.viewbox );
-    svg.style.width = viewBox[2] / viewBox[3] + 'em';
-    svg.style.height = '1em';
-    svg.innerHTML = svgData.content;
-
-    icon.parentNode.replaceChild( svg, icon );
-
-  }
-
-  addStyles() {
-
-    const style = document.createElement( 'style' );
-    style.innerHTML = `.${this.className} { display: inline-block; font-size: inherit; overflow: visible; vertical-align: -0.125em; preserveAspectRatio: none; }`;
-    document.head.appendChild( style );
-
-  }
-
-}
-
-const Icons = new IconsConverter( {
-
-  icons: {
-    settings: {
-      viewbox: '0 0 512 512',
-      content: '<path fill="currentColor" d="M444.788 291.1l42.616 24.599c4.867 2.809 7.126 8.618 5.459 13.985-11.07 35.642-29.97 67.842-54.689 94.586a12.016 12.016 0 0 1-14.832 2.254l-42.584-24.595a191.577 191.577 0 0 1-60.759 35.13v49.182a12.01 12.01 0 0 1-9.377 11.718c-34.956 7.85-72.499 8.256-109.219.007-5.49-1.233-9.403-6.096-9.403-11.723v-49.184a191.555 191.555 0 0 1-60.759-35.13l-42.584 24.595a12.016 12.016 0 0 1-14.832-2.254c-24.718-26.744-43.619-58.944-54.689-94.586-1.667-5.366.592-11.175 5.459-13.985L67.212 291.1a193.48 193.48 0 0 1 0-70.199l-42.616-24.599c-4.867-2.809-7.126-8.618-5.459-13.985 11.07-35.642 29.97-67.842 54.689-94.586a12.016 12.016 0 0 1 14.832-2.254l42.584 24.595a191.577 191.577 0 0 1 60.759-35.13V25.759a12.01 12.01 0 0 1 9.377-11.718c34.956-7.85 72.499-8.256 109.219-.007 5.49 1.233 9.403 6.096 9.403 11.723v49.184a191.555 191.555 0 0 1 60.759 35.13l42.584-24.595a12.016 12.016 0 0 1 14.832 2.254c24.718 26.744 43.619 58.944 54.689 94.586 1.667 5.366-.592 11.175-5.459 13.985L444.788 220.9a193.485 193.485 0 0 1 0 70.2zM336 256c0-44.112-35.888-80-80-80s-80 35.888-80 80 35.888 80 80 80 80-35.888 80-80z" />',
-    },
-    back: {
-      viewbox: '0 0 512 512',
-      content: '<path transform="translate(512, 0) scale(-1,1)" fill="currentColor" d="M503.691 189.836L327.687 37.851C312.281 24.546 288 35.347 288 56.015v80.053C127.371 137.907 0 170.1 0 322.326c0 61.441 39.581 122.309 83.333 154.132 13.653 9.931 33.111-2.533 28.077-18.631C66.066 312.814 132.917 274.316 288 272.085V360c0 20.7 24.3 31.453 39.687 18.164l176.004-152c11.071-9.562 11.086-26.753 0-36.328z" />',
-    },
-    trophy: {
-      viewbox: '0 0 576 512',
-      content: '<path fill="currentColor" d="M552 64H448V24c0-13.3-10.7-24-24-24H152c-13.3 0-24 10.7-24 24v40H24C10.7 64 0 74.7 0 88v56c0 66.5 77.9 131.7 171.9 142.4C203.3 338.5 240 360 240 360v72h-48c-35.3 0-64 20.7-64 56v12c0 6.6 5.4 12 12 12h296c6.6 0 12-5.4 12-12v-12c0-35.3-28.7-56-64-56h-48v-72s36.7-21.5 68.1-73.6C498.4 275.6 576 210.3 576 144V88c0-13.3-10.7-24-24-24zM64 144v-16h64.2c1 32.6 5.8 61.2 12.8 86.2-47.5-16.4-77-49.9-77-70.2zm448 0c0 20.2-29.4 53.8-77 70.2 7-25 11.8-53.6 12.8-86.2H512v16zm-127.3 4.7l-39.6 38.6 9.4 54.6c1.7 9.8-8.7 17.2-17.4 12.6l-49-25.8-49 25.8c-8.8 4.6-19.1-2.9-17.4-12.6l9.4-54.6-39.6-38.6c-7.1-6.9-3.2-19 6.7-20.5l54.8-8 24.5-49.6c4.4-8.9 17.1-8.9 21.5 0l24.5 49.6 54.8 8c9.6 1.5 13.5 13.6 6.4 20.5z" />',
-    },
-    cancel: {
-      viewbox: '0 0 352 512',
-      content: '<path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" />',
-    },
-    theme: {
-      viewbox: '0 0 512 512',
-      content: '<path fill="currentColor" d="M204.3 5C104.9 24.4 24.8 104.3 5.2 203.4c-37 187 131.7 326.4 258.8 306.7 41.2-6.4 61.4-54.6 42.5-91.7-23.1-45.4 9.9-98.4 60.9-98.4h79.7c35.8 0 64.8-29.6 64.9-65.3C511.5 97.1 368.1-26.9 204.3 5zM96 320c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm32-128c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm128-64c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm128 64c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32z"/>',
-    },
-    reset: {
-      viewbox: '0 0 512 512',
-      content: '<path fill="currentColor" d="M370.72 133.28C339.458 104.008 298.888 87.962 255.848 88c-77.458.068-144.328 53.178-162.791 126.85-1.344 5.363-6.122 9.15-11.651 9.15H24.103c-7.498 0-13.194-6.807-11.807-14.176C33.933 94.924 134.813 8 256 8c66.448 0 126.791 26.136 171.315 68.685L463.03 40.97C478.149 25.851 504 36.559 504 57.941V192c0 13.255-10.745 24-24 24H345.941c-21.382 0-32.09-25.851-16.971-40.971l41.75-41.749zM32 296h134.059c21.382 0 32.09 25.851 16.971 40.971l-41.75 41.75c31.262 29.273 71.835 45.319 114.876 45.28 77.418-.07 144.315-53.144 162.787-126.849 1.344-5.363 6.122-9.15 11.651-9.15h57.304c7.498 0 13.194 6.807 11.807 14.176C478.067 417.076 377.187 504 256 504c-66.448 0-126.791-26.136-171.315-68.685L48.97 471.03C33.851 486.149 8 475.441 8 454.059V320c0-13.255 10.745-24 24-24z" />',
-    },
-    trash: {
-      viewbox: '0 0 448 512',
-      content: '<path fill="currentColor" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z" />',
-    },
-  },
-
-  convert: true,
-
-} );
 
 const STATE = {
   Menu: 0,
@@ -3758,15 +3537,11 @@ class Game {
     this.transition.init();
 
     this.storage.loadGame();
-    // this.scores.calcStats();
 
     setTimeout( () => {
 
       this.transition.float();
       this.transition.cube( SHOW );
-
-      // setTimeout( () => this.transition.title( SHOW ), 700 );
-      // setTimeout( () => this.transition.buttons( BUTTONS.Menu, BUTTONS.None ), 1000 );
 
     }, 500 );
 
@@ -3816,44 +3591,6 @@ class Game {
 
     };
 
-    // this.dom.buttons.back.onclick = event => {
-
-    //   if ( this.transition.activeTransitions > 0 ) return;
-
-    //   if ( this.state === STATE.Playing ) {
-
-    //     this.game( HIDE );
-
-    //   } else if ( this.state === STATE.Prefs ) {
-
-    //     this.prefs( HIDE );
-
-    //   } else if ( this.state === STATE.Theme ) {
-
-    //     this.theme( HIDE );
-
-    //   }
-
-    // };
-
-    // this.dom.buttons.reset.onclick = event => {
-
-    //   if ( this.state === STATE.Theme ) {
-
-    //     this.themeEditor.resetTheme();
-
-    //   }
-      
-    // };
-
-    // this.dom.buttons.prefs.onclick = event => this.prefs( SHOW );
-
-    // this.dom.buttons.theme.onclick = event => this.theme( SHOW );
-
-    // this.dom.buttons.stats.onclick = event => this.stats( SHOW );
-
-    // this.controls.onSolved = () => this.complete( SHOW );
-
   }
 
   game( show ) {
@@ -3868,23 +3605,10 @@ class Game {
 
       }
 
-      const duration = this.saved ? 0 :
         this.scrambler.converted.length * ( this.controls.flipSpeeds[0] + 10 );
 
       this.state = STATE.Playing;
       this.saved = true;
-
-      // this.transition.buttons( BUTTONS.None, BUTTONS.Menu );
-
-      this.transition.zoom( STATE.Playing, duration );
-      // this.transition.title( HIDE );
-
-      setTimeout( () => {
-
-        // this.transition.timer( SHOW );
-        // this.transition.buttons( BUTTONS.Playing, BUTTONS.None );
-
-      }, this.transition.durations.zoom - 1000 );
 
       setTimeout( () => {
 
@@ -3914,170 +3638,8 @@ class Game {
 
   }
 
-  prefs( show ) {
-
-    if ( show ) {
-
-      if ( this.transition.activeTransitions > 0 ) return;
-
-      this.state = STATE.Prefs;
-
-      this.transition.buttons( BUTTONS.Prefs, BUTTONS.Menu );
-
-      // this.transition.title( HIDE );
-      this.transition.cube( HIDE );
-
-      setTimeout( () => this.transition.preferences( SHOW ), 1000 );
-
-    } else {
-
-      this.cube.resize();
-
-      this.state = STATE.Menu;
-
-      this.transition.buttons( BUTTONS.Menu, BUTTONS.Prefs );
-
-      this.transition.preferences( HIDE );
-
-      setTimeout( () => this.transition.cube( SHOW ), 500 );
-      // setTimeout( () => this.transition.title( SHOW ), 1200 );
-
-    }
-
-  }
-
-  theme( show ) {
-
-    this.themeEditor.colorPicker( show );
-    
-    if ( show ) {
-
-      if ( this.transition.activeTransitions > 0 ) return;
-
-      this.cube.loadFromData( States[ '3' ][ 'checkerboard' ] );
-
-      this.themeEditor.setHSL( null, false );
-
-      this.state = STATE.Theme;
-
-      this.transition.buttons( BUTTONS.Theme, BUTTONS.Prefs );
-
-      this.transition.preferences( HIDE );
-
-      setTimeout( () => this.transition.cube( SHOW, true ), 500 );
-      setTimeout( () => this.transition.theming( SHOW ), 1000 );
-
-    } else {
-
-      this.state = STATE.Prefs;
-
-      this.transition.buttons( BUTTONS.Prefs, BUTTONS.Theme );
-
-      this.transition.cube( HIDE, true );
-      this.transition.theming( HIDE );
-
-      setTimeout( () => this.transition.preferences( SHOW ), 1000 );
-      setTimeout( () => {
-
-        const gameCubeData = JSON.parse( localStorage.getItem( 'theCube_savedState' ) );
-
-        if ( !gameCubeData ) {
-
-          this.cube.resize( true );
-          return;
-
-        }
-
-        this.cube.loadFromData( gameCubeData );
-
-      }, 1500 );
-
-    }
-
-  }
-
-  stats( show ) {
-
-    if ( show ) {
-
-      if ( this.transition.activeTransitions > 0 ) return;
-
-      this.state = STATE.Stats;
-
-      this.transition.buttons( BUTTONS.Stats, BUTTONS.Menu );
-
-      // this.transition.title( HIDE );
-      this.transition.cube( HIDE );
-
-      setTimeout( () => this.transition.stats( SHOW ), 1000 );
-
-    } else {
-
-      this.state = STATE.Menu;
-
-      this.transition.buttons( BUTTONS.Menu, BUTTONS.None );
-
-      this.transition.stats( HIDE );
-
-      setTimeout( () => this.transition.cube( SHOW ), 500 );
-      // setTimeout( () => this.transition.title( SHOW ), 1200 );
-
-    }
-
-  }
-
-  complete( show ) {
-
-    if ( show ) {
-
-      this.transition.buttons( BUTTONS.Complete, BUTTONS.Playing );
-
-      this.state = STATE.Complete;
-      this.saved = false;
-
-      this.controls.disable();
-      this.timer.stop();
-      this.storage.clearGame();
-
-      this.bestTime = this.scores.addScore( this.timer.deltaTime );
-
-      this.transition.zoom( STATE.Menu, 0 );
-      this.transition.elevate( SHOW );
-
-      setTimeout( () => {
-
-        this.transition.complete( SHOW, this.bestTime );
-        this.confetti.start();
-
-      }, 1000 );
-
-    } else {
-
-      this.state = STATE.Stats;
-      this.saved = false;
-
-      this.transition.timer( HIDE );
-      this.transition.complete( HIDE, this.bestTime );
-      this.transition.cube( HIDE );
-      this.timer.reset();
-
-      setTimeout( () => {
-
-        this.cube.reset();
-        this.confetti.stop();
-
-        this.transition.stats( SHOW );
-        this.transition.elevate( 0 );
-
-      }, 1000 );
-
-      return false;
-
-    }
-
-  }
-
 }
 
+// CREATE GAME/CUBE
 // window.version = '0.99.2';
 // window.game = new Game();
