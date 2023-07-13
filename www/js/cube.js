405f98c25c6466b8db2b56b51bc594a25358dc01
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2023-07-13 16:26:24 +0800

    Remove shuffle, allow rotates from the start
    Shuffle was initially triggered on the start of the game (double click).
    Double click doesn't do anything now. Cube was also initially only
    rotatable upon starting the game. It can now be rotated immediately.

--- www/js/cube.js
+++ www/js/cube.js
@@ -1903,7 +1903,7 @@ class Transition {
 
   init() {
 
-    this.game.controls.disable();
+    // this.game.controls.disable();
 
     this.game.cube.object.position.y = this.data.cubeY;
     this.game.cube.animator.position.y = 4;
@@ -1911,49 +1911,17 @@ class Transition {
     this.game.world.camera.zoom = this.data.cameraZoom;
     this.game.world.camera.updateProjectionMatrix();
 
-    this.tweens.buttons = {};
-    this.tweens.timer = [];
+    // this.tweens.buttons = {};
+    // this.tweens.timer = [];
     // this.tweens.title = [];
-    this.tweens.best = [];
-    this.tweens.complete = [];
-    this.tweens.prefs = [];
-    this.tweens.theme = [];
-    this.tweens.stats = [];
+    // this.tweens.best = [];
+    // this.tweens.complete = [];
+    // this.tweens.prefs = [];
+    // this.tweens.theme = [];
+    // this.tweens.stats = [];
 
   }
 
-  // buttons( show, hide ) {
-
-  //   const buttonTween = ( button, show ) => {
-
-  //     return new Tween( {
-  //       target: button.style,
-  //       duration: 300,
-  //       easing: show ? Easing.Power.Out( 2 ) : Easing.Power.In( 3 ),
-  //       from: { opacity: show ? 0 : 1 },
-  //       to: { opacity: show ? 1 : 0 },
-  //       onUpdate: tween => {
-
-  //         const translate = show ? 1 - tween.value : tween.value;
-  //         button.style.transform = `translate3d(0, ${translate * 1.5}em, 0)`;
-
-  //       },
-  //       onComplete: () => button.style.pointerEvents = show ? 'all' : 'none'
-  //     } );
-
-  //   };
-
-  //   hide.forEach( button =>
-  //     this.tweens.buttons[ button ] = buttonTween( this.game.dom.buttons[ button ], false )
-  //   );
-
-  //   setTimeout( () => show.forEach( button => {
-
-  //     this.tweens.buttons[ button ] = buttonTween( this.game.dom.buttons[ button ], true );
-
-  //   } ), hide ? 500 : 0 );
-
-  // }
 
   cube( show, theming = false ) {
 
@@ -2431,172 +2399,6 @@ class Timer extends Animation {
   }
 
 }
-
-// const RangeHTML = [
-
-//   '<div class="range">',
-//     '<div class="range__label"></div>',
-//     '<div class="range__track">',
-//       '<div class="range__track-line"></div>',
-//       '<div class="range__handle"><div></div></div>',
-//     '</div>',
-//     '<div class="range__list"></div>',
-//   '</div>',
-
-// ].join( '\n' );
-
-// document.querySelectorAll( 'range' ).forEach( el => {
-
-//   const temp = document.createElement( 'div' );
-//   temp.innerHTML = RangeHTML;
-
-//   const range = temp.querySelector( '.range' );
-//   const rangeLabel = range.querySelector( '.range__label' );
-//   const rangeList = range.querySelector( '.range__list' );
-
-//   range.setAttribute( 'name', el.getAttribute( 'name' ) );
-//   rangeLabel.innerHTML = el.getAttribute( 'title' );
-
-//   if ( el.hasAttribute( 'color' ) ) {
-
-//     range.classList.add( 'range--type-color' );
-//     range.classList.add( 'range--color-' + el.getAttribute( 'name' ) );
-
-//   }
-
-//   if ( el.hasAttribute( 'list' ) ) {
-
-//     el.getAttribute( 'list' ).split( ',' ).forEach( listItemText => {
-
-//       const listItem = document.createElement( 'div' );
-//       listItem.innerHTML = listItemText;
-//       rangeList.appendChild( listItem );
-
-//     } );
-
-//   }
-
-//   el.parentNode.replaceChild( range, el );
-
-// } );
-
-// class Range {
-
-//   constructor( name, options ) {
-
-//     options = Object.assign( {
-//       range: [ 0, 1 ],
-//       value: 0,
-//       step: 0,
-//       onUpdate: () => {},
-//       onComplete: () => {},
-//     }, options || {} );
-
-//     // this.element = document.querySelector( '.range[name="' + name + '"]' );
-//     // this.track = this.element.querySelector( '.range__track' );
-//     // this.handle = this.element.querySelector( '.range__handle' );
-//     // this.list = [].slice.call( this.element.querySelectorAll( '.range__list div' ) );
-
-//     this.value = options.value;
-//     this.min = options.range[0];
-//     this.max = options.range[1];
-//     this.step = options.step;
-
-//     this.onUpdate = options.onUpdate;
-//     this.onComplete = options.onComplete;
-
-//     // this.setValue( this.value );
-
-//     this.initDraggable();
-
-//   }
-
-//   // setValue( value ) {
-
-//   //   this.value = this.round( this.limitValue( value ) );
-//   //   this.setHandlePosition();
-
-//   // }
-
-//   initDraggable() {
-
-//     let current;
-
-//     this.draggable = new Draggable( this.handle, { calcDelta: true } );
-
-//     this.draggable.onDragStart = position => {
-
-//       current = this.positionFromValue( this.value );
-//       this.handle.style.left = current + 'px';
-
-//     };
-
-//     this.draggable.onDragMove = position => {
-
-//       current = this.limitPosition( current + position.delta.x );
-//       this.value = this.round( this.valueFromPosition( current ) );
-//       this.setHandlePosition();
-      
-//       this.onUpdate( this.value );
-
-//     };
-
-//     this.draggable.onDragEnd = position => {
-
-//       this.onComplete( this.value );
-
-//     };
-
-//   }
-
-//   round( value ) {
-
-//     if ( this.step < 1 ) return value;
-
-//     return Math.round( ( value - this.min ) / this.step ) * this.step + this.min;
-
-//   }
-
-//   limitValue( value ) {
-
-//     const max = Math.max( this.max, this.min );
-//     const min = Math.min( this.max, this.min );
-
-//     return Math.min( Math.max( value, min ), max );
-
-//   }
-
-//   limitPosition( position ) {
-
-//     return Math.min( Math.max( position, 0 ), this.track.offsetWidth );
-
-//   }
-
-//   percentsFromValue( value ) {
-
-//     return ( value - this.min ) / ( this.max - this.min );
-
-//   }
-
-//   valueFromPosition( position ) {
-
-//     return this.min + ( this.max - this.min ) * ( position / this.track.offsetWidth );
-
-//   }
-
-//   positionFromValue( value ) {
-
-//     return this.percentsFromValue( value ) * this.track.offsetWidth;
-
-//   }
-
-//   // setHandlePosition() {
-
-//   //   this.handle.style.left = this.percentsFromValue( this.value ) * 100 + '%';
-
-//   // }
-
-// }
 
 class Preferences {
 
@@ -3579,13 +3381,13 @@ class Game {
 
       if ( ! this.saved ) {
 
-        this.scrambler.scramble();
-        this.controls.scrambleCube();
+        // this.scrambler.scramble();
+        // this.controls.scrambleCube();
         this.newGame = true;
 
       }
 
-        this.scrambler.converted.length * ( this.controls.flipSpeeds[0] + 10 );
+        // this.scrambler.converted.length * ( this.controls.flipSpeeds[0] + 10 );
 
       this.state = STATE.Playing;
       this.saved = true;
