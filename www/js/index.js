489ab4d525b1590a0564a1dc47258f0ad79d61ea
Shin I. Lim <92615780+ShinayLim@users.noreply.github.com>
2023-07-30 23:48:52 +0800

    ud feedback

--- www/js/index.js
+++ www/js/index.js
@@ -1410,31 +1410,60 @@ function onBackButton() {
 }
 
 // ================= FEEDBACK FORM =================
+const contactForm = document.getElementById('contact-form'),
+  contactName = document.getElementById('contact-name'),
+  contactEmail = document.getElementById('contact-email'),
+  contactSubject = document.getElementById('contact-subject'),
+  contactMessage = document.getElementById('contact-message'),
+  errorMessage = document.getElementById('error-message');
 
-function sendEmail(subject, body, recipients, ccList, bccList) {
-    // Check if the plugin is available
-    if (window.plugins && window.plugins.emailComposer) {
-        window.plugins.emailComposer.showEmailComposerWithCallback(
-            function(result) {
-                if (result === 0) {
-                    console.log("Email sent successfully.");
-                } else {
-                    console.log("Email not sent.");
-                }
-            },
-            subject,
-            body,
-            recipients,
-            ccList,
-            bccList,
-            false, // isHTML (true: body is HTML, false: body is plain text)
-            null, // attachments (file paths or base64 data)
-            null, // attachment names
-            null, // attachment types
-        );
-    } else {
-        console.log("The 'emailComposer' plugin is not available.");
-    }
-}
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // check if the field has a value
+  if (
+    contactName.value === '' ||
+    contactEmail.value === '' ||
+    contactSubject.value === '' ||
+    contactMessage.value === ''
+  ) {
+    // show message
+    errorMessage.textContent = 'Write all the input fields';
+  } else {
+    // serviceID - templateID - #form - publickey
+    emailjs
+      .sendForm(
+        'service_sda6v3o',
+        'template_v51gr8c',
+        '#contact-form',
+        'o3tWIaBrjgDdAoT8d'
+      )
+      .then(
+        () => {
+          // show message and add color, window + dot to open emoji
+          // it will just prompt a "Message Sent" text but it will not basically
+          // be sent to my email because it needs to use use a server-side scripting language like PHP
+          errorMessage.classList.add('color-first');
+          errorMessage.textContent = 'Message sent ✔';
+
+          // remove message after 5 seconds
+          setTimeout(() => {
+            errorMessage.textContent = '';
+          }, 5000);
+        },
+        (error) => {
+          alert('WAIT MAY MALI!!!!!!!!!!!', error);
+        }
+      );
+
+    // clear input fields
+    contactName.value = '';
+    contactEmail.value = '';
+    contactSubject.value = '';
+    contactMessage.value = '';
+  }
+};
+
+contactForm.addEventListener('submit', sendEmail);
 
 
